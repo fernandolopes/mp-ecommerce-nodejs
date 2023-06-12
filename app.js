45199fc92be973e2a633a90644d3ac0e17542615
Fernando Lopes da Silva <fernandolopes.s@gmail.com>
2023-06-12 09:42:44 -0300

    add notification

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
+
+// SDK do Mercado Pago
+const mercadopago = require('mercadopago');
+// Adicione as credenciais
+mercadopago.configure({
+  access_token: 'APP_USR-2453313229452572-092911-2df2d24eb035a4c0852f3455a89d1459-1160953381'
+});
+
 
 var app = express();
  
@@ -15,8 +23,65 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/checkout', function (req, res){
+    console.log(req.query);
+    res.render('checkout', req.query);
+          
+});
+
+app.post('/notification', function (req, res){
+    console.warn(req.body);
+    res.send("aqui");
+});
+
+app.get('/notification', function (req, res){
+    console.warn(req.body);
+    res.send("aqui");
+});
+
+app.put('/notification', function (req, res){
+    console.warn(req.body);
+    res.send("aqui");
+});
+
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+
+    const { query } = req;
+    const back_urls = `${req.protocol}://${req.headers.host}/checkout`;
+    const notification_url = `${req.protocol}://${req.headers.host}/notification`;
+    let preference = {
+        items: [
+            {
+                title: query.title,
+                picture_url: `${req.protocol}://${req.headers.host}/${query.img.replace(/^\.\//,'')}`,
+                unit_price: Number.parseInt(query.price,0.0),
+                quantity: Number.parseInt(query.unit,0)
+            }
+        ],
+        notification_url: notification_url,
+        back_urls: {
+            success: back_urls,
+            pending: back_urls,
+            failure: back_urls,
+        },
+        payment_methods: {
+            installments: 6,
+            default_installments: 1
+        },
+    };
+
+    console.log(preference);
+
+    mercadopago.preferences.create(preference)
+        .then(function(response){
+            // Este valor substituirá a string "<%= global.id %>" no seu HTML
+            global.id = response.body.id;
+            res.render('detail', {id: response.body.id, ...req.query});
+            
+        }).catch(function(error){
+            console.log(error);
+        });
+
 });
 
 app.listen(port);
